ad5ad2e5f137969b97d45604145cf371bf7f939d
LuisEQ <luiseduardoql@hotmail.com>
2022-12-05 01:56:18 -0700

    Product service correct url

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -23,34 +23,34 @@ export class ProductService {
   }
     
   getProducts(): Observable <Product[]>{
-    return this.http.get<Product[]>(`${this.productsUrl}/getall.php`)
+    return this.http.get<Product[]>(`${this.productsUrl}/getallProduct.php`)
     .pipe(
       tap(_ => this.log('Actualizado productos')),
       catchError(this.handleError<Product[]>('getProducts', []))
     );
   }
   getProduct(id: number): Observable<Product> {
-    const url = `${this.productsUrl}/get.php?idProduct=${id}`;
+    const url = `${this.productsUrl}/getProduct.php?idProduct=${id}`;
     return this.http.get<Product>(url).pipe(
       tap(_ => this.log(`fetched product id=${id}`)),
       catchError(this.handleError<Product>(`getProduct id=${id}`))
     );
   }
   updateProduct(product: Product){
-    return this.http.put(`${this.productsUrl}/update.php`, product).pipe(
+    return this.http.put(`${this.productsUrl}/updateProduct.php`, product).pipe(
       tap(_ => this.log(`updated id=${product.id}`)),
       catchError(this.handleError<any>('updateProduct'))
     );
   }
 
   addProduct(product: Product){
-    return this.http.post<Product>(`${this.productsUrl}/post.php`, product).pipe(
+    return this.http.post<Product>(`${this.productsUrl}/postProduct.php`, product).pipe(
       tap((newProduct: Product) => this.log(`added Product w/ id=${newProduct.id}`)),
       catchError(this.handleError<Product>('addProduct'))
     );
   }
   deleteProduct(id: number){
-    const url = `${this.productsUrl}/delete.php?idProduct=${id}`;
+    const url = `${this.productsUrl}/deleteProduct.php?idProduct=${id}`;
 
     return this.http.delete<Product>(url).pipe(
       tap(_ => this.log(`deleted product id=${id}`)),
