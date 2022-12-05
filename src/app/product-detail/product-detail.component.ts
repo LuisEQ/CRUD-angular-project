a4a38b9370e418b427bbb93a38f679fb22b5e022
LuisEQ <luiseduardoql@hotmail.com>
2022-12-05 03:30:58 -0700

    Add product details and changed type input number

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { ProductService } from '../services/product.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() product?: Product;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getProduct();
   }
-
+  getProduct(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.productService.getProduct(id)
+      .subscribe(product => this.product = product);
+  }
+  goBack(): void {
+    this.location.back();
+  }
+  save(): void {
+    if (this.product) {
+      this.productService.updateProduct(this.product)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
